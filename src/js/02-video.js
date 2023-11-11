0652ffeaedd9e28045a8916755e63a9d240b1c2b
Iryna Holova <gis.golova@gmail.com>
2023-11-11 18:51:52 +0200

    add common file

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,14 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+import common from './common';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const currentTime = localStorage.getItem(common.LS_KEY_PLAYER) ?? 0;
+
 player.on('timeupdate', throttle(saveCurrentTime, 1000));
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time') ?? 0);
+player.setCurrentTime(currentTime);
 
 function saveCurrentTime({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
---
+++ src/js/common.js
@@ -0,0 +1,5 @@
+const common = {
+  LS_KEY_PLAYER: 'videoplayer-current-time',
+};
+
+export default common;
